9d182d45f782108e787b1ab7bcfa5618f396f1c4
geng <geng@com>
2022-08-26 18:49:50 +0800

    first commit

---
+++ cocos-js/wait-for-ammo-instantiation.js
@@ -0,0 +1,13 @@
+System.register(['./instantiated-d661b08f.js'], function (exports) {
+	'use strict';
+	return {
+		setters: [function (module) {
+			exports('default', module.hw);
+		}],
+		execute: function () {
+
+
+
+		}
+	};
+});
